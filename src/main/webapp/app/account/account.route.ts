dd3a074ef39cbb06f7be295c82561795d730b755
Joseph <48005710+joeyave@users.noreply.github.com>
2020-08-08 08:01:49 +0300

    Revert "remove registration for anonymous users, add password field to admin'…"

--- src/main/webapp/app/account/account.route.ts
+++ src/main/webapp/app/account/account.route.ts
@@ -4,9 +4,10 @@ import { activateRoute } from './activate/activate.route';
 import { passwordRoute } from './password/password.route';
 import { passwordResetFinishRoute } from './password-reset/finish/password-reset-finish.route';
 import { passwordResetInitRoute } from './password-reset/init/password-reset-init.route';
+import { registerRoute } from './register/register.route';
 import { settingsRoute } from './settings/settings.route';
 
-const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetFinishRoute, passwordResetInitRoute, settingsRoute];
+const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetFinishRoute, passwordResetInitRoute, registerRoute, settingsRoute];
 
 export const accountState: Routes = [
   {
--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,18 +5,10 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
-import { PasswordStrengthBarComponent } from 'app/account/password/password-strength-bar.component';
 
 @NgModule({
   imports: [MentalCentralSharedLibsModule],
-  declarations: [
-    FindLanguageFromKeyPipe,
-    AlertComponent,
-    AlertErrorComponent,
-    LoginModalComponent,
-    HasAnyAuthorityDirective,
-    PasswordStrengthBarComponent,
-  ],
+  declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
   entryComponents: [LoginModalComponent],
   exports: [
     MentalCentralSharedLibsModule,
@@ -25,7 +17,6 @@ import { PasswordStrengthBarComponent } from 'app/account/password/password-stre
     AlertErrorComponent,
     LoginModalComponent,
     HasAnyAuthorityDirective,
-    PasswordStrengthBarComponent,
   ],
 })
 export class MentalCentralSharedModule {}
