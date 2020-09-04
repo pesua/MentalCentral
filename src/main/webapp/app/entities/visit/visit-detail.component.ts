4e692048226a33c47e5a5642315a5d5ada86d56c
Alex2001Shurik <57028313+Alex2001Shurik@users.noreply.github.com>
2020-09-04 23:15:14 +0300

    creating and downloading docx file

--- src/main/webapp/app/entities/visit/visit-detail.component.ts
+++ src/main/webapp/app/entities/visit/visit-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlignmentType, Document, Packer, Paragraph, TabStopPosition, TabStopType, TextRun } from 'docx';
+
+import { saveAs } from 'file-saver';
 
 import { IVisit } from 'app/shared/model/visit.model';
 
@@ -18,5 +21,113 @@ export class VisitDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+
+  creatingAndDownloadingPDF(): void {
+    const doc = new Document();
+
+    doc.addSection({
+      children: [
+        new Paragraph({
+          text: 'Обласная клиническая больница им. И.И. Мечникова',
+          alignment: AlignmentType.CENTER,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          tabStops: [
+            {
+              type: TabStopType.RIGHT,
+              position: TabStopPosition.MAX,
+            },
+          ],
+          children: [
+            new TextRun({
+              text: 'Тел 0988944992, 0991582275',
+            }),
+            new TextRun({
+              text: '\tУкраина, 49005',
+            }),
+          ],
+        }),
+        new Paragraph({
+          tabStops: [
+            {
+              type: TabStopType.RIGHT,
+              position: TabStopPosition.MAX,
+            },
+          ],
+          children: [
+            new TextRun({
+              text: 'Факс: (0562) 36-09-67',
+            }),
+            new TextRun({
+              text: '\tг. Днепр',
+            }),
+          ],
+        }),
+        new Paragraph({
+          spacing: {
+            after: 50,
+          },
+          tabStops: [
+            {
+              type: TabStopType.RIGHT,
+              position: TabStopPosition.MAX,
+            },
+          ],
+          children: [
+            new TextRun({
+              text: 'e-mail: psookbm@gmail.com',
+            }),
+            new TextRun({
+              text: '\tпл. Соборная, 14',
+            }),
+          ],
+        }),
+        new Paragraph({
+          text: 'Консультировал врач ' + this.visit!.user!.lastName + ' ' + this.visit!.user!.firstName,
+          alignment: AlignmentType.CENTER,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          text: 'Б-ой: ' + this.visit!.patient!.fullName,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          text: 'Прож: ' + this.visit!.patient!.address,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          text: 'Д-з: ' + this.visit!.patient!.diagnosis,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          text: 'Рекомендовано: ' + this.visit!.therapy,
+          spacing: {
+            after: 50,
+          },
+        }),
+        new Paragraph({
+          text: new Date().getDate().toString() + '.' + (new Date().getMonth() + 1).toString() + '.' + new Date().getFullYear().toString(),
+          spacing: {
+            after: 50,
+          },
+        }),
+      ],
+    });
+
+    Packer.toBlob(doc).then(blob => {
+      saveAs(blob, this.visit!.patient!.fullName + '.docx');
+    });
   }
 }
