fd68899f62d6b0f7f82fffc43331e729cbdce63a
Alex2001Shurik <57028313+Alex2001Shurik@users.noreply.github.com>
2020-09-16 18:16:53 +0300

    fix doctor's page

---
+++ src/main/webapp/app/entities/visit/filter-visits-by-date.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Moment } from 'moment';
+import { IVisit } from '../../shared/model/visit.model';
+import * as moment from 'moment';
+
+@Pipe({
+  name: 'filterVisitsByDate',
+})
+export class FilterVisitsByDatePipe implements PipeTransform {
+  private date!: Moment;
+  transform(items: IVisit[]): IVisit[] {
+    this.date = moment();
+    if (!items) {
+      return [];
+    }
+
+    return items.filter(item => this.date.diff(item.time, 'hour') <= 12);
+  }
+}
